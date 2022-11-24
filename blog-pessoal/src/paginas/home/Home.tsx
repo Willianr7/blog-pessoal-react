c6fd08ba0e8a06886012c7886d98b62ab4220ff2
Willianr7 <rafael.net@hotmail.com.br>
2022-11-24 10:17:24 -0300

    atualizacao

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+let nome = 'Will'
+
+function Home(){
+    return(
+        <div>
+        <h1 className='fonte'> Willian R</h1>
+        <h2>{nome}</h2>
+        <p className='fonte'>Turma 2</p>
+
+         </div>
+
+    );
+
+}
+
+export default Home;
